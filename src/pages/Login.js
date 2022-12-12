2c9f2c36f29d6808ad04488c3b56de34f0d05cc9
Luiza Ogura <luizapogura@gmail.com>
2022-12-12 19:14:33 -0300

    requisito 1 feito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { submitLogin } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = () => {
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+    dispatch(submitLogin(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const emailValid = /^[\w.+]+@\w+.\w{2,}(?:.\w{2})?$/gmi;
+    const passwordValid = 6;
+    const isValid = (emailValid.test(email)) && (password.length >= passwordValid);
+
+    return (
+      <div>
+        <p>Login</p>
+        <form>
+          <input
+            label="Insira seu email:"
+            type="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+            value={ email }
+            name="email"
+          />
+          <input
+            label="Senha:"
+            type="text"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+            value={ password }
+            name="password"
+          />
+          <button
+            type="button"
+            disabled={ !isValid }
+            onClick={ this.handleSubmit }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
